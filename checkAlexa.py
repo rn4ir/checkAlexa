c9800a72c78d9120e3127629066357d5a247eb01
rn4ir <rn@rahul-nair.com>
2017-09-21 11:43:32 -0400

    added argument parser

--- checkAlexa.py
+++ checkAlexa.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 import urllib.request
+import argparse
 import zipfile
 import csv
 
 rankings = {}
-
+"""
 url = 'http://s3.amazonaws.com/alexa-static/top-1m.csv.zip'
 alexa_zipfile = 'top-1m.csv.zip'
 alexa_csvfile = 'top-1m.csv'
@@ -19,5 +20,18 @@ csv_reader = csv.reader(open(alexa_csvfile, 'r'))
 for row in csv_reader:
     key, value = row
     rankings[key] = value
+"""
+cli_argparser = argparse.ArgumentParser(description='')
+cli_argparser.add_argument('-n', '--number', type=int, help="Displays the top 'n' Alexa rankings.", required=False)
+cli_argparser.add_argument('-q', '--query', help="Checks a website's current ranking.", required=False)
+cli_argparser.add_argument('-o', '--outfile', help="Saves the output to an external file.", required=False)
+cli_args = cli_argparser.parse_args()
 
-print (rankings)
+if (cli_args.number):
+    print (cli_args.number)
+elif (cli_args.query):
+    print (cli_args.query)
+elif (cli_args.outfile):
+    print (cli_args.outfile)
+else:
+    print ("Else default")
