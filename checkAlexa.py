642528a85cb649eef3d5912d98c400a7aa455c14
rn4ir <rn@rahul-nair.com>
2017-09-22 18:43:06 -0400

    added -n output

--- checkAlexa.py
+++ checkAlexa.py
@@ -4,6 +4,7 @@ import urllib.request
 import argparse
 import zipfile
 import csv
+from itertools import islice
 
 rankings = {}
 
@@ -11,15 +12,22 @@ url = 'http://s3.amazonaws.com/alexa-static/top-1m.csv.zip'
 alexa_zipfile = 'top-1m.csv.zip'
 alexa_csvfile = 'top-1m.csv'
 
+"""
 urllib.request.urlretrieve(url, alexa_zipfile)
 
 with zipfile.ZipFile(alexa_zipfile, 'r') as alexazip:
     alexazip.extractall()
+"""
 
 csv_reader = csv.reader(open(alexa_csvfile, 'r'))
 for row in csv_reader:
     key, value = row
     rankings[key] = value
+
+def topn_domains(num):
+    firstn_list = list(islice(rankings.items(), num))
+    for item in firstn_list:
+        print ("Rank #" + str(item[0]) + "\t\t" + item[1])
 
 cli_argparser = argparse.ArgumentParser(description='')
 cli_argparser.add_argument('-n', '--number', type=int, help="Displays the top 'n' Alexa rankings.", required=False)
@@ -29,9 +37,11 @@ cli_args = cli_argparser.parse_args()
 
 if (cli_args.number):
     print (cli_args.number)
+    topn_domains(cli_args.number)
 elif (cli_args.query):
     print (cli_args.query)
 elif (cli_args.outfile):
     print (cli_args.outfile)
 else:
     print ("Else default")
+    topn_domains(50)
